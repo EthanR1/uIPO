713928007396e68f6815cbd95d4ae9182ce2bbe4
Ethan Reich <ethandreich@gmail.com>
2025-08-14 19:45:53 -0600

    Set Vite base to /uIPO/ for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/uIPO-website/',   // if your repo is YOUR_USERNAME.github.io, use base: '/'
+  base: '/uIPO/',   // match your repo name EXACTLY (case matters)
 })
